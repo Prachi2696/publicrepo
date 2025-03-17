5e33b1861e77820a63d68714d371fccdf22d01eb
Prachi2696 <prachibhudeo26@gmail.com>
2025-03-17 16:28:58 +0530

    Updates

---
+++ precache-manifest.6c6fc1d005fb75f327f9a90cf444a378.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2178eabff26eaff0ef7b82720707184f",
+    "url": "/publicrepo/index.html"
+  },
+  {
+    "revision": "69bd2f36c2cadb653b81",
+    "url": "/publicrepo/static/css/main.a78a9cf9.chunk.css"
+  },
+  {
+    "revision": "01d1f6ab3f7c86e89dd3",
+    "url": "/publicrepo/static/js/2.519c2ea6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/publicrepo/static/js/2.519c2ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69bd2f36c2cadb653b81",
+    "url": "/publicrepo/static/js/main.bf14fa3c.chunk.js"
+  },
+  {
+    "revision": "eb405fbdbe9da0e3b014",
+    "url": "/publicrepo/static/js/runtime-main.1f6e2e8e.js"
+  },
+  {
+    "revision": "f3e1419df2a1ac00aadeb8d1b62fe4c7",
+    "url": "/publicrepo/static/media/bell.f3e1419d.mp3"
+  },
+  {
+    "revision": "cba35ec083544ce496cbc8455bb1b44e",
+    "url": "/publicrepo/static/media/click.cba35ec0.mp3"
+  },
+  {
+    "revision": "6050cb4aa4e41474305cd61bfdf4cca7",
+    "url": "/publicrepo/static/media/github.6050cb4a.svg"
+  }
+]);
